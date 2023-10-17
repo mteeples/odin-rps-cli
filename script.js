9f99d6102e45e00ae4f03fa832c75f3465f10245
Michael Teeples <m.teeples@co-dx.com>
2023-10-17 17:13:23 -0600

    Implement getPlayerChoice

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // getComputerChoice()
 // randomly returns rock, paper, or scissors
 
+// getPlayerChoice()
+// prompt for selection. 
+// convert to lowercase. 
+// Verify that input is valid, otherwise re-prompt
+function getPlayerChoice() {
+    const VALID_CHOICES = [
+        "rock",
+        "paper",
+        "scissors"
+    ];
+    let userInput = prompt("Enter your choice").toLowerCase();
+    let keepGoing = true;
+    while (keepGoing) {
+        if (VALID_CHOICES.includes(userInput)) {
+            return userInput;
+        } else {
+            userInput = prompt(
+                "Invalid response. Please enter rock, paper, or scissors"
+            ).toLowerCase()
+        }
+    }
+}
+
 // playRound(playerSelection, computerSelection) -> String declaring the winner "You lose! Paper beats rock"
-function playRound() {
+function playRound(playerSelection, computerSelection) {
     return "You lose! Paper beats Rock";
 }
 
@@ -11,7 +34,7 @@ function playRound() {
 function game() {
     console.log("Welcome to Rock, Paper, Scissors");
     for (let i = 0; i < 5; i++) {
-        console.log(playRound());
+        console.log(playRound(getPlayerChoice(), "rock"));
     }
     console.log("Thank you for playing");
 }
